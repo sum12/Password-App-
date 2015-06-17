94959b8092e68b4c6343fdfb535629aa76d5662e
Diptanshu8 <diptanshuj@gmail.com>
2015-06-18 00:13:09 +0530

    Enryption and Decryption Methods completed Substition cipher

--- Password App/Cipher/cipher.py
+++ Password App/Cipher/cipher.py
@@ -1,13 +1,38 @@
-
-class BaseCipher(object)
+import random
+class BaseCipher(object):
     def encrypt(s):
         raise NotImplementedError
     def decrypt(s):
         raise NotImplementedError
 
-
 class PassThroughCipher(BaseCipher):
-    def encrypt(s):
-        return s:
-    def descrypt(s):
-        return s
+    def __init__(self):
+        self.r = []
+    def encrypt(self,s):
+        t = ''
+        for line in s:
+            for l in line:
+                if l == '':
+                    t = t + chr(32)
+                else:
+                    x = random.randint(33,126)
+                    t = t + chr(ord(l) + x)
+                    self.r.append(x)          
+        return t
+    def decrypt(self,s):
+        t = ''
+        for line in s:
+            for l in line:
+                if l == '':
+                    t = t + chr(32)
+                else:
+                    t = t + chr(ord(l) - self.r[0])
+                    print self.r
+                    self.r = self.r[1:]
+        return t
+            
+
+a = PassThroughCipher()
+x = raw_input()
+i = a.encrypt(x)
+j = a.decrypt(i)
