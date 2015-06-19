58863a41bbcc85939a6d5627a09e1edd23e8e5a4
Diptanshu8 <diptanshuj@gmail.com>
2015-06-19 23:26:47 +0530

    -Everything except crypting has been done -the program wont run if u arent a user as it wont bypass authentication
    -only one thing left that is to code it for multiple users
    -and ofcourse crypting is left

--- Password App/password.py
+++ Password App/password.py
@@ -29,11 +29,153 @@ class main(wx.Frame):
         self.Show()
 
     def savenewpassword(self,e):
-        t = npass(None,"New Entry")
+        a = npass(None,"Save your password")
+        
     def reviewpassword(self,e):
-        t = rpass(None,"Review your Credentials")
+        a = rpass(None,"Review your saved passwords")
+        
     def quit(self,e):
         self.Close()
+
+class authentication(wx.Frame):
+    def __init__(self,parent,title):
+        wx.Frame.__init__(self,parent,title = title)
+        panel = wx.Panel(self,-1)
+        
+        vsizer = wx.BoxSizer(wx.VERTICAL)
+
+        hsizer1 = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer3 = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer_b = wx.BoxSizer(wx.HORIZONTAL)
+        
+        st1 = wx.StaticText(panel,-1,"Enter your Name \n(First + Last name) :")
+        self.sb1 = wx.TextCtrl(panel,-1)
+        hsizer1.Add(st1,1,wx.RIGHT|wx.ALIGN_CENTER_HORIZONTAL,10)
+        hsizer1.Add(self.sb1,2,0,0)
+
+        st2 = wx.StaticText(panel,-1,"Enter the phone number \n(with which u registered the account) :")
+        self.sb2 = wx.TextCtrl(panel,-1)
+        hsizer2.Add(st2,1,wx.RIGHT|wx.ALIGN_CENTER_HORIZONTAL,0)
+        hsizer2.Add(self.sb2,2,0,0)
+
+        self.b1 = wx.Button(panel,1,"Login")
+        self.b1.Bind(wx.EVT_BUTTON,self.login)
+        self.b2 = wx.Button(panel,1,"Exit")
+        self.b2.Bind(wx.EVT_BUTTON,self.quit)
+        self.b3 = wx.Button(panel,1,"New User")
+        self.b3.Bind(wx.EVT_BUTTON,self.user_reg)
+
+        hsizer_b.Add(self.b1,0,wx.ALIGN_CENTER_HORIZONTAL,0)
+        hsizer_b.Add(self.b2,0,wx.ALIGN_CENTER_HORIZONTAL,0)
+        hsizer_b.Add(self.b3,0,wx.ALIGN_CENTER_HORIZONTAL,0)
+        
+        vsizer.Add(hsizer1,2,wx.ALIGN_CENTER_HORIZONTAL,0)
+        vsizer.Add(hsizer2,2,wx.ALIGN_CENTER_HORIZONTAL,0)
+        vsizer.Add(hsizer_b,2,wx.ALIGN_CENTER_HORIZONTAL|wx.UP,10)
+
+        self.current_user = 0
+        panel.SetSizer(vsizer)
+        self.Centre()
+        self.Show()
+        
+    def login(self,e):
+        self.userinfo()
+        i = self.user_match()
+        if i == 1:
+            t = main(None,"Welcome " + self.username_list[self.current_user])
+            self.quit(e)
+        else:
+            self.st1 = wx.StaticText(self,-1,"Either of your credentials are wrong\n Kindly make changes and retry with login",(80,115),style = wx.ALIGN_CENTRE)
+            
+    def userinfo(self):
+        f = open('C:\Users\DJ\Documents\pss.txt','r')
+        self.username_list,self.usernumber_list = [],[]
+        for l in f:
+            temp = str.split(str(l),'\t')
+            if temp[0] == 'Username':
+                self.username_list.append(str(temp[1]))
+                self.usernumber_list.append(str(temp[2]))
+                
+    
+    def user_match(self):
+        temp1 = str(self.sb1.GetValue())
+        temp2 = str(self.sb2.GetValue())
+        i = 0
+        while i<len(self.username_list):
+            if temp1 == str(self.username_list[i]) and temp2 == str(self.usernumber_list[i]):
+                self.current_user = i
+                return 1;
+            i+=1
+        return 0
+                
+    def user_reg(self,e):
+        a = user_regestration(None,"Register yourself user")
+        
+    
+    def quit(self,e):
+        self.Close()
+    
+"""        st3 = wx.StaticText(panel,-1,"Securtiy question")
+        self.sb3 = wx.TextCtrl(panel,-1)
+        vsizer.Add(st3,1,wx.RIGHT,70)
+        vsizer.Add(self.sb3,2,0,0)
+"""
+##########################################################################################################################################################################
+class user_regestration(wx.Frame):
+    def __init__(self,parent,title):
+        wx.Frame.__init__(self,parent,title = title)
+        panel = wx.Panel(self,-1)
+        vsizer = wx.BoxSizer(wx.VERTICAL)
+
+        hsizer1 = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer3 = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer_b = wx.BoxSizer(wx.HORIZONTAL)
+
+        st1 = wx.StaticText(panel,-1,"Enter your Name \n(First + Last name) :")
+        self.sb1 = wx.TextCtrl(panel,-1)
+        hsizer1.Add(st1,1,wx.RIGHT|wx.ALIGN_CENTER_HORIZONTAL,10)
+        hsizer1.Add(self.sb1,2,0,0)
+
+        st2 = wx.StaticText(panel,-1,"Enter the phone number \n(with which you want to register your account) :")
+        self.sb2 = wx.TextCtrl(panel,-1)
+        hsizer2.Add(st2,1,wx.RIGHT|wx.ALIGN_CENTER_HORIZONTAL,0)
+        hsizer2.Add(self.sb2,2,0,0)
+
+        self.b1 = wx.Button(panel,1,"Save")
+        self.b1.Bind(wx.EVT_BUTTON,self.save)
+        self.b2 = wx.Button(panel,1,"Exit")
+        self.b2.Bind(wx.EVT_BUTTON,self.quit)
+
+        hsizer_b.Add(self.b1,0,wx.ALIGN_CENTER_HORIZONTAL,0)
+        hsizer_b.Add(self.b2,0,wx.ALIGN_CENTER_HORIZONTAL,0)
+        
+        vsizer.Add(hsizer1,2,wx.ALIGN_CENTER_HORIZONTAL,0)
+        vsizer.Add(hsizer2,2,wx.ALIGN_CENTER_HORIZONTAL,0)
+        vsizer.Add(hsizer_b,2,wx.ALIGN_CENTER_HORIZONTAL,0)
+        #vsizer.Add(hsizer1,2,0,0)
+
+        panel.SetSizer(vsizer)
+        self.Centre()
+        self.Show()
+
+    def save(self,e):
+        f = open('C:\Users\DJ\Documents\pss.txt','a')
+        f.write("Username")
+        f.write('\t')
+        f.write(str(self.sb1.GetValue()))
+        f.write('\t')
+        f.write(str(self.sb2.GetValue()))
+        f.write('\t')
+        f.write('\n')
+        f.close()
+        self.Close()
+             
+    def quit(self,e):
+        self.Close()
+
+##########################################################################################################################################################################
 
 class npass(wx.Frame):
     def __init__(self,parent,title):
@@ -91,12 +233,13 @@ class npass(wx.Frame):
         f.write('\n')
         f.close()
         self.Close()
-        ##self.st1.SetLabel(str(temp[1]))
-        ##self.st2.SetLabel(str(temp[2]))
                 
         
     def quit(self,e):
             self.Close()
+
+##########################################################################################################################################################################
+
 class rpass(wx.Frame):
 
     def __init__(self,parent,title):
@@ -106,7 +249,8 @@ class rpass(wx.Frame):
         string = []
         for l in f:
             temp = str.split(str(l),'\t')
-            string.append(temp[0])
+            if str(temp[0]) != 'Username':
+                string.append(temp[0])
         f.close()
 
         vsizer = wx.BoxSizer(wx.VERTICAL)
@@ -148,9 +292,10 @@ class rpass(wx.Frame):
                 f.close()
                 return;
         f.close()
-
     def close(self,e):
             self.Close()
+
+##########################################################################################################################################################################
 app = wx.App()
-aa = main(None,"Remeber your passwords with ease")
+aa = authentication(None,"Remeber your passwords with ease")
 app.MainLoop()
