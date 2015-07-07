76e7a5c5793e0da8674073ee97ebceff9c73ffa4
Diptanshu8 <diptanshuj@gmail.com>
2015-07-07 18:30:13 +0530

    Edit password functionality working but filters are required to be added making the app more presentable is required

--- Password App/filehandlingmod.py
+++ Password App/filehandlingmod.py
@@ -3,7 +3,7 @@ import string
 class filehandling():
     def userinfo(self):
         f = open('C:\Users\DJ\Documents\pss.txt','r')
-        userinfo = [tuple([(l.split('\t'))[2],(l.split('\t'))[3]]) for l in f if (l.split('\t'))[1] == 'Username' and (l.split('\t'))[0] == '#u' and len(l.split('\t')) >= 3]
+        userinfo = [tuple([(l.split('\t'))[2],(l.split('\t'))[3]]) for l in f if (l.split('\t'))[1] == 'Username' and (l.split('\t'))[0] == '#u' and len(l.split('\t')) >= 2]
         return userinfo
     
     def save_new_user(self,x,y):
@@ -37,3 +37,16 @@ class filehandling():
         if len(f)>0:
             return 1
         else: return 0
+   
+    def save_editted_password(self,user,x,old,new):
+        f = open('C:\Users\DJ\Documents\pss.txt','r+')
+        s= ""        
+        for l in f:
+            t = l.split("\t")
+            if  (user) in t and (x) in t and (old) in t : 
+                t[t.index((old))] = str(new)
+            s = s  + "\t".join(t)
+        f.seek(0)
+        f.write(s)
+        f.close()
+        return
